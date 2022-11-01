5f8f5ae9e782cc8d06489f964d6ff26b3ec39a34
Ignacia Silva <ignaciasilva1@gmail.com>
2022-11-01 18:19:55 -0300

    abono a cuenta corriente

--- index.js
+++ index.js
@@ -4,29 +4,48 @@ class Cliente
 {
     nombreCliente;
     dniCliente;
-    numeroCuenta;
-    saldoCuenta;
 }
+
+
+class CuentaCorriente
+{
+    numero;
+    saldo;
+
+    depositoEnCuenta(valor){
+        this.saldo += valor; //en vez de hacer una función para cada cuenta con el fin de aceptar depósitos, se utiliza this. Es decir si una cuenta recibe depósito, se suma el inicial el valor  
+    }
+}
+
+
 
 const cliente1 = new Cliente();
 cliente1.nombreCliente = "José";
 cliente1.dniCliente = "13232";
-cliente1.numeroCuenta = "23232323";
-cliente1.saldoCuenta = 2000;
+
+const cuentaCorriente1 = new CuentaCorriente();
+cuentaCorriente1.numero = "23232323";
+cuentaCorriente1.saldo = 5000;
 
 const cliente2 = new Cliente();
 cliente2.nombreCliente = "María";
 cliente2.dniCliente = "12332";
-cliente2.numeroCuenta = "567832323";
-cliente2.saldoCuenta = 2000;
+
+const cuentaCorriente2 = new CuentaCorriente();
+cuentaCorriente2.numero = "567832323";
+cuentaCorriente2.saldo = 2000;
+
+console.log(cuentaCorriente2.saldo);
+cuentaCorriente2.depositoEnCuenta(100); //se suma los 100 al saldo inicial de la cuenta (despues de linea 16)
+console.log(cuentaCorriente2.saldo);
+cuentaCorriente2.depositoEnCuenta(500); 
+console.log(cuentaCorriente2.saldo);
 
 const cliente3 = new Cliente();
 cliente3.nombreCliente = "Leonardo";
 cliente3.dniCliente = "134456789";
-cliente3.numeroCuenta ="12343434"
-cliente3.saldoCuenta = 1000;
 
+const cuentaCorriente3 = new CuentaCorriente();
+cuentaCorriente3.numero = "12343434"
+cuentaCorriente3.saldo = 1000;
 
-console.log(cliente1);
-console.log(cliente2);
-console.log(cliente3);
